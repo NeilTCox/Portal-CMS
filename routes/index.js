d16e808c304b3528a5645fc82e96da685c45c213
Neil Schultz-Cox <neiltcox@icloud.com>
2018-01-19 12:04:25 -0800

    Implemented user registration, login, and adding new pages to DB

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ function toLower (str) {
 }
 
 var userSchema = mongoose.Schema({
-  email: {type: String, set: toLower},
+  email: {type: String, set: toLower, unique: true},
   password: String
 });
 var userModel = mongoose.model('users', userSchema);
@@ -26,7 +26,10 @@ router.post('/auth/register', function(req, res){
     password: req.body.password,
   })
   newUser.save(function(err, user){
-    if (err) return console.error(err);
+    if(err){
+      res.redirect('/auth');
+      return console.error(err);
+    }
     res.redirect('/admin');
   });
 });
@@ -36,16 +39,34 @@ router.post('/auth/login', function(req, res){
     email: req.body.email,
     password: req.body.password,
   })
+  userModel.findOne({ 'email': req.body.email }, function (err, password) {
+    if (err) {
+      console.error(err);
+      console.log('THERE HAS BEEN AN ERROR. USER NOT IN DB?')
+      res.redirect('/auth');
+    }else if (password.toObject().password == req.body.password) {
+      console.log('SUCCESS!');
+      res.redirect('/admin');
+    }else {
+      console.log('PASSWORD INCORRECT');
+      res.redirect('/auth');
+    }
+  });
 
 });
 
 router.post('/admin/addpage', function(req, res){
-  var newUser = new userModel({
-    email: req.body.email,
-    password: req.body.password,
+  var newPage = new pageModel({
+    title: req.body.title,
+    content: req.body.main_content,
+    url: req.body.url,
+    template: req.body.template,
   })
-  newUser.save(function(err, user){
-    if (err) return console.error(err);
+  newPage.save(function(err, user){
+    if(err){
+      res.redirect('/admin/addpage');
+      return console.error(err);
+    }
     res.redirect('/admin');
   });
 });
